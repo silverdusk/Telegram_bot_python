8d974cebedb178cda0dbcfd61cb1f9f1fc38f78d
silverdusk <kiryxa@gmail.com>
2024-02-23 20:19:58 +0000

    Moved db logic from bot.py to database.py

--- bot.py
+++ bot.py
@@ -15,13 +15,6 @@ logging.basicConfig(filename='log.log', level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logging.info("Starting bot successfully at: " + str(datetime.datetime.utcnow()) + " UTC")
 
-if load_dotenv():
-    logging.info(".env file loaded successfully")
-    print(".env file loaded successfully")
-else:
-    logging.error("Error loading .env file")
-    print("Error loading .env file")
-
 # BOT_TOKEN = os.environ.get('BOT_TOKEN')
 BOT_TOKEN = os.getenv('BOT_TOKEN', )
 bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
@@ -32,73 +25,15 @@ DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_TABLE_NAME = os.getenv('DB_TABLE_NAME')
+
 ALLOWED_TYPES = ['spare part', 'miscellaneous']
 
-
-def create_connection():
-    conn = None
-    try:
-        conn = psycopg2.connect(
-            dbname=DB_NAME,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            host=DB_HOST,
-            port=DB_PORT
-        )
-        logging.info("Connected to the database")
-        print("Connected to the database")
-    except psycopg2.Error as e:
-        logging.error(f"Database connection error: {e}")
-        print(f"Error: {e}")
-    return conn
-
-
-def insert_data_into_postgres(conn, message, request):
-    cursor = conn.cursor()
-    try:
-        # Construct the SQL INSERT query
-        sql_query = f"""INSERT INTO {DB_NAME}.{DB_TABLE_NAME} (item_name, item_amount, item_type, item_price, 
-                                                                            availability, chat_id) 
-                       VALUES (%s, %s, %s, %s, %s, %s)"""
-
-        # Execute the SQL query with the data from the request object
-        cursor.execute(sql_query, (
-            request.item_name, request.item_amount, request.item_type, request.item_price, request.availability,
-            message.chat.id))
-
-        # Commit the transaction
-        conn.commit()
-        logging.info("Data inserted into Postgres database")
-        print("Data inserted into Postgres database")
-    except psycopg2.Error as e:
-        logging.error(f"Error inserting data into Postgres database: {e}")
-        print(f"Error inserting data into Postgres database: {e}")
-        conn.rollback()
-    finally:
-        if cursor:
-            cursor.close()
-
-
-def update_availability_in_database(conn, item, availability):
-    cursor = conn.cursor()
-    try:
-        # Construct the SQL UPDATE query
-        sql_query = f"""UPDATE {DB_NAME}.{DB_TABLE_NAME} 
-                                SET availability = %s 
-                                WHERE item = %s"""
-
-        # Execute the SQL query to update availability status
-        cursor.execute(sql_query, (availability, item))
-
-        # Commit the transaction
-        conn.commit()
-        print("Availability status updated in Postgres database")
-    except psycopg2.Error as e:
-        print(f"Error updating availability status in Postgres database: {e}")
-        conn.rollback()
-    finally:
-        if cursor:
-            cursor.close()
+if load_dotenv():
+    logging.info(".env file loaded successfully")
+    print(".env file loaded successfully")
+else:
+    logging.error("Error loading .env file")
+    print("Error loading .env file")
 
 
 def is_int(string):
@@ -250,11 +185,11 @@ def check_availability_item(message, item):
                          f'Item {item}. \n'
                          f'Availability - not available')
         # Establish connection to Postgres database
-        conn = create_connection()
+        conn = database.create_connection()
         if conn is not None:
             try:
                 # Update availability status in Postgres database
-                update_availability_in_database(conn, item, True)
+                database.update_availability_in_database(conn, item, True)
             finally:
                 # Close the connection to the Postgres database
                 if conn:
@@ -265,11 +200,11 @@ def check_availability_item(message, item):
                          f'Item {item}. \n'
                          f'Availability - not available')
         # Establish connection to Postgres database
-        conn = create_connection()
+        conn = database.create_connection()
         if conn is not None:
             try:
                 # Update availability status in Postgres database
-                update_availability_in_database(conn, item, False)
+                database.update_availability_in_database(conn, item, False)
             finally:
                 # Close the connection to the Postgres database
                 if conn:
@@ -370,11 +305,11 @@ def request_decision_handler(message, request):
 
 def ok_request(message, request):
     # Establish connection to Postgres database
-    conn = create_connection()
+    conn = database.create_connection()
     if conn is not None:
         try:
             # Insert data into Postgres database
-            insert_data_into_postgres(conn, message, request)
+            database.insert_data_into_postgres(conn, message, request)
 
             # Construct the text message to send
             text = f'Request is placed for processing:{os.linesep}' \
